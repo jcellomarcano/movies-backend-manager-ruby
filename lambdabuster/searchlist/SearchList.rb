ebd9057d80f23be5fe7e9ba0265ec5729c1c4e2d
mafermazu <mafer.mazu8@gmail.com>
2021-03-24 23:00:10 -0400

    [updated] Main base ready

---
+++ lambdabuster/searchlist/SearchList.rb
@@ -0,0 +1,18 @@
+class SearchList
+    def i n i t i a l i z e ( ? ? ? )
+    ???
+    end
+    def <<(elem )
+    @ l i s t << elem
+    self
+    end
+    def to_s
+    ???
+    end
+    def +( o t h e r )
+    ???
+    end
+    def each ( ∗ a r g s , &b l o c k )
+    @ l i s t . each ( ∗ a r g s , &b l o c k )
+    end
+    end
