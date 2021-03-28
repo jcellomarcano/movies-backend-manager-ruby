939989a71947837087114a91c7e235f31c355dad
Jesus Marcano <jcellomarcano@gmail.com>
2021-03-27 23:46:04 -0400

    director parse to list

--- lambdabuster/client/JsonInterpreter.rb
+++ lambdabuster/client/JsonInterpreter.rb
@@ -1,10 +1,21 @@
 require 'json'
 require_relative '../models/Person'
+require_relative '../models/SearchList'
 line = "./test.json"
-file = File.read(line)
-data_hash = JSON.parse(file)
-for director in data_hash['directors'] 
-    puts director
+def readJson(path) 
+    directorsList = SearchList.new()
+    file = File.read(path)
+    data_hash = JSON.parse(file)
+    for director in data_hash['directors'] 
+        auxDir = PersonType::Director.new(director['name'],Date.parse(director['birthday']), director['nationality'])
+        directorsList << auxDir
+    end
+    # puts directorsList
+    return directorsList
 end
+
+searchListica = readJson(line)
+puts searchListica
+
 
 
