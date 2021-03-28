02264a419ed4459fc0716979c13c8757c682094f
Jesus Marcano <jcellomarcano@gmail.com>
2021-03-28 01:32:46 -0400

    implemented error handling and actors add list

--- lambdabuster/client/JsonInterpreter.rb
+++ lambdabuster/client/JsonInterpreter.rb
@@ -4,18 +4,38 @@ require_relative '../models/SearchList'
 line = "./test.json"
 def readJson(path) 
     directorsList = SearchList.new()
+    actorsList = SearchList.new()
+    moviesList = SearchList.new()
     file = File.read(path)
     data_hash = JSON.parse(file)
-    for director in data_hash['directors'] 
-        auxDir = PersonType::Director.new(director['name'],Date.parse(director['birthday']), director['nationality'])
-        directorsList << auxDir
-    end
+    result = true
+    begin  # "try" block
+        for director in data_hash['directors'] 
+            auxDir = PersonType::Director.new(director['name'],Date.parse(director['birthday']), director['nationality'])
+            directorsList << auxDir
+        end
+        for actor in data_hash['actors']
+            auxAct = PersonType::Actor.new(actor['name'],Date.parse(actor['birthday']), actor['nationality'])
+            actorsList << auxAct
+        end
+    rescue # optionally: `rescue Exception => ex`
+        result =  false
+        raise "Error to load JSON"
+    ensure # will always get executed
+        if result 
+            return directorsList,actorsList
+        else 
+            return false        
+        end
+    end 
+    
     # puts directorsList
-    return directorsList
+    
 end
 
-searchListica = readJson(line)
-puts searchListica
+dir,act = readJson(line)
+puts dir 
+puts act
 
 
 
